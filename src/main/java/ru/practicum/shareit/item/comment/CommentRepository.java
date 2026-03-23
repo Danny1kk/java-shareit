b0ff3bfdebc387450f0a51e020cf1b7b089dfcca
Danny <danny.koshik@gmail.com>
2026-03-23 20:16:59 +0300

    Fix: Checkstyle violations

--- src/main/java/ru/practicum/shareit/item/comment/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/comment/CommentRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findAllByItemIn(List<Item> items);
-    
+
     List<Comment> findAllByItemId(Long itemId);
 }
