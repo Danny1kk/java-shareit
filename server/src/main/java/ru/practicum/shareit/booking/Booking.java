029900e70908ad5cba436ff7f08f3362d15a91c7
Danny <danny.koshik@gmail.com>
2026-04-16 01:51:12 +0300

    feat: implement item requests and implements gateway

---
+++ server/src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -0,0 +1,35 @@
+package ru.practicum.shareit.booking;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.User;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "bookings")
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "booker_id", nullable = false)
+    private User booker;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endTime;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private BookingStatus status;
+}
